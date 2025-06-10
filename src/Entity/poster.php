e2420eb4aafb2f88ad2fbea3eeba53f01130dd56
Eliott <eliott.betry@etudiant.univ-reims.fr>
2025-06-10 14:50:14 +0200

    ajout de la methode findbyid

--- src/Entity/poster.php
+++ src/Entity/poster.php
@@ -2,6 +2,9 @@
 declare(strict_types=1);
 
 namespace Entity;
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+
 class poster
 {
     private int $id;
@@ -15,6 +18,22 @@ class poster
     public function getId(): int
     {
         return $this->id;
+    }
+    public static function findById(int $id): genre
+    {
+        $stmt = MyPDO::getInstance()->prepare(<<<'SQL'
+                                                SELECT id, mediumblob
+                                                FROM   poster
+                                                WHERE id = :posterId
+                                                SQL);
+        $stmt->execute([':posterId' => $id]);
+        $poster = $stmt->fetchObject(poster::class);
+        if ($poster === false){
+            throw new EntityNotFoundException("ID du jeu non trouvé");
+        }
+        else {
+            return $poster;
+        }
     }
 
 }
