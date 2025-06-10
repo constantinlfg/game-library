525cce9e14434fca0dbc68578d6f12811092ee8c
Eliott <eliott.betry@etudiant.univ-reims.fr>
2025-06-10 11:30:41 +0200

    Initialisation de la class genre

---
+++ src/Entity/genre.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class genre
+{
+    private int $id;
+    private string $description;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+}
