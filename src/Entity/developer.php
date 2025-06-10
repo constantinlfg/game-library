e3c9e3302aea63ea8ab3a2ee2e12078cee72313a
Eliott <eliott.betry@etudiant.univ-reims.fr>
2025-06-10 11:59:39 +0200

    Initialisation de la class developer

---
+++ src/Entity/developer.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+namespace Entity;
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+
+class developer
+{
+    private int $id;
+    private string $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+
+}
