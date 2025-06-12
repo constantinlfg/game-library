1542326ae66b4289d59eb61027f549215814eae8
Constantin Lyfoung <constantin.lyfoung@etudiant.univ-reims.fr>
2025-06-12 11:30:15 +0200

    correction des url

--- public/category.php
+++ public/category.php
@@ -20,7 +20,7 @@ try {
     foreach ($jeux as $i) {
         $content = <<<HTML
 <div class="gameBox">
-  <a href="game.php/?gameId={$i->getId()}" class="game" style="text-decoration:none">
+  <a href="game.php?gameId={$i->getId()}" class="game" style="text-decoration:none">
     <img src="poster.php?posterId={$i->getPosterId()}">
     <div class="nomDesc">
       {$i->getName()} ({$i->getReleaseYear()})<p></p>
