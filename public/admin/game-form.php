9f21bd3af4628290e3e7aa2b9299a4c68b02e63d
Constantin Lyfoung <constantin.lyfoung@etudiant.univ-reims.fr>
2025-06-13 13:22:37 +0200

    ajout de game-form et game-save

---
+++ public/admin/game-form.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Entity\Game;
+use Html\Form\GameForm;
+use Entity\Exception\EntityNotFoundException;
+use Entity\Exception\ParameterException;
+
+try{
+    if(isset($_GET['gameId']) && is_numeric($_GET['gameId'])){
+        $form=new GameForm(Game::findById(intval($_GET['gameId'])));
+    }else{
+        $form=new GameForm();
+    }
+    echo $form->getHtmlForm('game-save.php');
+
+} catch (EntityNotFoundException) {
+http_response_code(404);
+} catch (Exception) {
+http_response_code(500);
+}
